df8537954e01aeae0c29f6f52b55ac797f4f0ff1
João Textor <joaotextor@gmail.com>
2023-04-16 12:18:12 -0300

    update close icon styles

--- assets/components/CloseIcon.styles.js
+++ assets/components/CloseIcon.styles.js
@@ -1,7 +1,9 @@
-export const styles = {
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
   viewCloseIcon: {
     marginTop: 20,
     marginRight: -15,
     alignItems: "flex-end",
   },
-};
+})
